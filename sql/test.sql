3a61cea5d4c28fec5b0411d6ebc039cc16054db1
ppg_ivan@yahoo.com <ppg_ivan@yahoo.com>
2019-10-22 08:28:09 +0800

    additional dummy data

--- sql/test.sql
+++ sql/test.sql
@@ -14,7 +14,10 @@ SELECT "Populating Seminars" AS MESSAGE;
 
 INSERT INTO rpfp.`rpfp_class`
             (`RPFP_CLASS_ID`, `TYPE_CLASS_ID`, `OTHERS_SPECIFY`,`BARANGAY_ID`,`CLASS_NUMBER`,`DATE_CONDUCTED`,`DB_USER_ID`)
-     VALUES (1, 1, NULL, 083747125, 'RPFP-TAC-2019-00001','2019-02-11', 'test')
+     VALUES (1, 1, NULL, 083747125, 'RPFP-TAC-2019-00001','2019-02-11', 'test'),
+            (2, 2, NULL, 083747125, 'RPFP-TAC-2019-00001','2019-02-11', 'test'),
+            (3, 2, NULL, 083747125, 'RPFP-TAC-2019-00002','2019-05-11', 'test'),
+            (4, 2, NULL, 083747125, 'RPFP-TAC-2019-00003','2019-09-11', 'test')
 ;
 
 -- --------------------------------------------------------
@@ -41,7 +44,35 @@ INSERT INTO rpfp.`couples`
             (12, 1, '13 ISANG STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 3, '2019-03-01', 0 ),
             (13, 1, '12 ISANG STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 7, '2019-03-01', 2 ),
             (14, 1, '11 ISANG STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 2 ),
-            (15, 1, '15 ISANG STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 0 )
+            (15, 1, '15 ISANG STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 0 ),
+            (16, 2, '2 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 5, '2019-03-01', 0 ),
+            (17, 2, '3 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 4, '2019-03-01', 2 ),
+            (18, 2, '4 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 3, '2019-03-01', 0 ),
+            (19, 2, '1 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 2 ),
+            (20, 2, '7 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 6, '2019-03-01', 2 ),
+            (21, 2, '6 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 0 ),
+            (22, 2, '9 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 1, '2019-03-01', 0 ),
+            (23, 2, '8 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 0, '2019-03-01', 0 ),
+            (24, 2, '10 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 5, '2019-03-01', 0 ),
+            (25, 2, '14 DALAWA STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 4, '2019-03-01', 0 ),
+            (26, 3, '13 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 3, '2019-03-01', 0 ),
+            (27, 3, '12 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 7, '2019-03-01', 2 ),
+            (28, 3, '11 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 2 ),
+            (29, 3, '15 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 0 ),
+            (30, 3, '2 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 5, '2019-03-01', 0 ),
+            (31, 3, '3 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 4, '2019-03-01', 2 ),
+            (32, 3, '4 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 3, '2019-03-01', 0 ),
+            (33, 3, '1 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 2 ),
+            (34, 3, '7 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 6, '2019-03-01', 2 ),
+            (35, 3, '6 TATLO STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 0 ),
+            (36, 4, '9 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 1, '2019-03-01', 0 ),
+            (37, 4, '8 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 0, '2019-03-01', 0 ),
+            (38, 4, '10 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 5, '2019-03-01', 0 ),
+            (39, 4, '14 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 4, '2019-03-01', 0 ),
+            (40, 4, '13 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 3, '2019-03-01', 0 ),
+            (41, 4, '12 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 7, '2019-03-01', 2 ),
+            (42, 4, '11 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 2 ),
+            (43, 4, '15 APAT STREET', 'PINAGKAISAHAN', 'MANDALUYONG', '1-000-102453-2', 2, '2019-03-01', 0 )
 ;
 
 SELECT "Populating Individuals" AS MESSAGE;
@@ -87,7 +118,48 @@ INSERT INTO rpfp.`individual`
             (22, 12, 'Isip',       'Angela',           '', '', 18,       2,        '2001-10-10', 2, 7, 1 ),
             (23, 13, 'Jimenez',    'Lylia',            '', '', 14,       2,        '2005-09-19', 2, 4, 1 ),
             (24, 14, 'Coronel',    'Hilda',            '', '', 47,       2,        '1972-02-02', 3, 7, 1 ),
-            (25, 15, 'Lopez',      'Karina',           '', '', 40,       2,        '1979-12-10', 4, 8, 1 )
+            (25, 15, 'Lopez',      'Karina',           '', '', 40,       2,        '1979-12-10', 4, 8, 1 ),
+            (26, 16, 'Aldana',      'Alejandro',      '', '', 38,       1,        '1981-09-09', 1, 8, 1 ),
+            (27, 17, 'Aragon',  'Bayani',           '', '', 30,       1,        '1989-01-07', 1, 7, 1 ),
+            (28, 17, 'Aragon',  'Rosamie',       '', '', 30,       2,        '1989-12-09', 1, 6, 1 ),
+            (29, 18, 'Bautista',      'Crisanto',   '', '', 28,       1,        '1991-05-07', 1, 5, 1 ),
+            (30, 18, 'Bautista',      'Jasmine',        '', '', 30,       2,        '1989-06-09', 1, 6, 1 ),
+            (31, 19, 'Borbon',    'Vedasto',         '', '', 34,       1,        '1985-04-07', 1, 6, 1 ),
+            (32, 19, 'Borbon',    'Blessica',           '', '', 31,       2,        '1988-10-09', 1, 6, 1 ),
+            (33, 20, 'Calalang',  'Joselito',          '', '', 27,       1,        '1992-11-07', 1, 7, 1 ),
+            (34, 20, 'Calalang',  'Tala',            '', '', 25,       2,        '1994-12-09', 1, 8, 1 ),
+            (35, 21, 'Celestial',    'Jaslene',           '', '', 28,       2,        '1991-07-07', 1, 9, 1 ),
+            (36, 21, 'Celestial',    'Rizalino',              '', '', 28,       1,        '1991-08-09', 1, 8, 1 ),
+            (37, 22, 'Diaz',  'Benilda',             '', '', 49,       2,        '1970-03-07', 1, 7, 1 ),
+            (38, 22, 'Diaz',  'Ernesto',            '', '', 47,       1,        '1972-03-09', 1, 7, 1 ),
+            (39, 23, 'Duran',     'Keanu',            '', '', 50,       1,        '1970-03-07', 1, 4, 1 ),
+            (40, 23, 'Duran',     'Floribeth',           '', '', 50,       2,        '1972-03-09', 1, 5, 0 ),
+            (41, 24, 'Espina',    'Chesa',            '', '', 25,       2,        '1994-06-07', 5, 5, 1 ),
+            (42, 25, 'Flores',     'Vergel',        '', '', 29,       1,        '1990-08-07', 5, 3, 0 ),
+            (43, 26, 'Gopez',     'Marisol',             '', '', 30,       2,        '1989-04-07', 5, 5, 1 ),
+            (44, 27, 'Hipolito',      'Antonio',           '', '', 30,       1,        '1989-01-07', 5, 5, 0 ),
+            (45, 28, 'Isidro',      'Liezel',           '', '', 21,       2,        '1998-11-05', 2, 8, 1 ),
+            (46, 29, 'Labrador',       'Luzviminda',           '', '', 18,       2,        '2001-10-10', 2, 7, 1 ),
+            (47, 30, 'Liwanag',    'Nenita',            '', '', 14,       2,        '2005-09-19', 2, 4, 1 ),
+            (48, 31, 'Magallanes',    'Rubylyn',            '', '', 47,       2,        '1972-02-02', 3, 7, 1 ),
+            (49, 32, 'Manzano',      'Luningning',           '', '', 40,       2,        '1979-12-10', 4, 8, 1 ),
+            (29, 33, 'Moreno',      'Juan',   '', '', 28,       1,        '1991-05-07', 1, 5, 1 ),
+            (30, 33, 'Moreno',      'Lualhati',        '', '', 30,       2,        '1989-06-09', 1, 6, 1 ),
+            (31, 34, 'Natividad',    'Rodrigo',         '', '', 34,       1,        '1985-04-07', 1, 6, 1 ),
+            (32, 34, 'Natividad',    'Amor',           '', '', 31,       2,        '1988-10-09', 1, 6, 1 ),
+            (33, 35, 'Oliveros',  'Efren',          '', '', 27,       1,        '1992-11-07', 1, 7, 1 ),
+            (34, 35, 'Oliveros',  'Perlah',            '', '', 25,       2,        '1994-12-09', 1, 8, 1 ),
+            (35, 36, 'Pascua',    'Reyna',           '', '', 28,       2,        '1991-07-07', 1, 9, 1 ),
+            (36, 36, 'Pascua',    'Mauricio',              '', '', 28,       1,        '1991-08-09', 1, 8, 1 ),
+            (37, 37, 'Posadas',  'Sampaguita',             '', '', 49,       2,        '1970-03-07', 1, 7, 1 ),
+            (38, 37, 'Posadas',  'Fernandez',            '', '', 47,       1,        '1972-03-09', 1, 7, 1 ),
+            (39, 38, 'Ricarte',     'Venancio',            '', '', 50,       1,        '1970-03-07', 1, 4, 1 ),
+            (40, 38, 'Ricarte',     'Mahalia',           '', '', 50,       2,        '1972-03-09', 1, 5, 0 ),
+            (41, 39, 'Rosario',    'Darna',            '', '', 25,       2,        '1994-06-07', 5, 5, 1 ),
+            (42, 40, 'Soledad',     'Melchor',        '', '', 29,       1,        '1990-08-07', 5, 3, 0 ),
+            (43, 41, 'Sangcap',     'Barbara',             '', '', 30,       2,        '1989-04-07', 5, 5, 1 ),
+            (44, 42, 'Tizon',      'Danilo',           '', '', 30,       1,        '1989-01-07', 5, 5, 0 ),
+            (45, 43, 'Venosa',      'Adah',           '', '', 21,       2,        '1998-11-05', 2, 8, 1 )
 ;
 
 
@@ -113,7 +185,60 @@ INSERT INTO rpfp.`fp_details`
             (12, 12, NULL, NULL, 6, 4, NULL),
             (13, 13, NULL, NULL, 6, 4, NULL),
             (14, 14, NULL, NULL, 6, 4, NULL),
-            (15, 15, NULL, NULL, 6, 4, NULL)
+            (15, 15, NULL, NULL, 6, 4, NULL),
+            (16, 16, NULL, NULL, 1, 4, NULL),
+            (17, 17, 3, 2, NULL, NULL, NULL),
+            (18, 18, NULL, NULL, 6, 4, NULL),
+            (19, 19, NULL, NULL, 6, 3, NULL),
+            (20, 20, 4, NULL, 6, 1, 1),
+            (21, 21, NULL, NULL, 1, 2, NULL),
+            (22, 22, 12, NULL, NULL, NULL, NULL),
+            (23, 23, 5, NULL, 2, 1, 2),
+            (24, 24, 5, NULL, 6, 1, 2),
+            (25, 25, NULL, NULL, 6, 4, NULL),
+            (26, 26, NULL, NULL, 6, 4, NULL),
+            (27, 27, NULL, NULL, 6, 4, NULL),
+            (28, 28, NULL, NULL, 6, 4, NULL),
+            (29, 29, NULL, NULL, 6, 4, NULL),
+            (30, 30, NULL, NULL, 1, 4, NULL),
+            (31, 31, 3, 2, NULL, NULL, NULL),
+            (32, 32, NULL, NULL, 6, 4, NULL),
+            (33, 33, NULL, NULL, 6, 3, NULL),
+            (34, 34, 4, NULL, 6, 1, 1),
+            (35, 35, NULL, NULL, 1, 2, NULL),
+            (36, 36, 12, NULL, NULL, NULL, NULL),
+            (37, 37, 5, NULL, 2, 1, 2),
+            (38, 38, 5, NULL, 6, 1, 2),
+            (39, 39, NULL, NULL, 6, 4, NULL),
+            (40, 40, NULL, NULL, 6, 4, NULL),
+            (41, 41, NULL, NULL, 6, 4, NULL),
+            (42, 42, NULL, NULL, 6, 4, NULL),
+            (43, 43, NULL, NULL, 6, 4, NULL)
+;
+
+SELECT "Populating details of target couples per month" AS MESSAGE;
+-- --------------------------------------------------------
+--
+-- Dumping data for table `fp_details`
+--
+
+INSERT INTO rpfp.`target_couples`
+            (`TARGET_ID`, `TARGET_YEAR`, `TARGET_MONTH`, `PSGC_CODE`, `TARGET_COUNT`)
+     VALUES (1, 2019, 01, 08, 1000),
+            (2, 2019, 02, 08, 1000),
+            (3, 2019, 03, 08, 1000),
+            (4, 2019, 04, 08, 1000),
+            (5, 2019, 05, 08, 1000),
+            (6, 2019, 06, 08, 1000),
+            (7, 2019, 07, 08, 1000),
+            (8, 2019, 08, 08, 1000),
+            (9, 2019, 09, 08, 1000),
+            (10, 2019, 10, 08, 1000),
+            (11, 2019, 11, 08, 1000),
+            (12, 2019, 12, 08, 1000),
+            (13, 2019, 01, 02, 1000),
+            (14, 2019, 02, 02, 1000),
+            (15, 2019, 03, 02, 1000),
 ;
 
 -- --------------------------------------------------------
