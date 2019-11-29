c497dc56bfb3027bec27349b28faa98173b540f7
ppg_ivan@yahoo.com <ppg_ivan@yahoo.com>
2019-11-29 18:22:38 +0800

    updated dashboard content and landing design

--- application/views/index/landingPage.php
+++ application/views/index/landingPage.php
@@ -19,6 +19,13 @@
             </div>
         </nav>
 
+<?php
+
+$current_year = date('Y');
+$current_month = date('M');
+$current_day = date('d');
+?>
+
         <section class="bg-img">
             <div class="container">
                 <div class="row">
@@ -29,58 +36,214 @@
                 </div>
             </div>
 
-            <div class="container padding-b5p">
+
+            <div class="container margin-t7 statisticsReport" >
+    <div class="row">    
+        <div class="col-md-12 col-sm-12 col-xs-12 border-1"> 
+            <div class="col-md-12 col-sm-12 col-xs-12">   
                 <div class="row">
-                    <div class="col-xs-12"> 
-                        <h3 class="ora">Announcements</h3>
-                        <p class="small">News and updates from the POPCOM head office.</p>
+                    <div class="col-sm-12">
+                        <br>
+                        <h4><center>Statistical Snapshot</center></h4>
+                        <br>
+                    </div>
+                </div> 
+            </div>
+            
+            <div class="col-md-6 col-sm-12 col-xs-12"> 
+                <div class="row">
+                    <div class="col-xs-9 col-sm-9">
+                        <p>Couples encoded as of FY <?= $current_year; ?></p>
+                        <p>Couples encoded as of Jan 1 to <?= $current_month; ?> <?= $current_day; ?>, <?= $current_year; ?></p>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 text-right">
+                        <p>1,563,385</p>
+                        <p>270,532</p>
                     </div>
                 </div>
-                <div class="row announce-margin">
-                    <div class="col-xs-12" style="padding: 0">
-                        <div class="col-xs-1 text-right" style="padding-left: 0; width: 1%">
-                            <span><i class="fa fa-bookmark-o"></i></span>
-                        </div>
-                        <div class="col-xs-9">
-                            <p class="small">September 3, 2019</p>
-                            <p class="small flex">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation
-                            </p>
-                        </div>
-                        <div class="col-xs-2"></div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <br>
+                        <h5>Type of Participant for Yr <?= $current_year; ?></h5>
                     </div>
-                    <div class="col-xs-12 margin-t15" style="padding: 0">
-                        <div class="col-xs-1 text-right" style="padding-left: 0; width: 1%">
-                            <span><i class="fa fa-bookmark-o"></i></span>
-                        </div>
-                        <div class="col-xs-9">
-                            <p class="small">September 3, 2019</p>
-                            <p class="small flex">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation
-                            </p>
-                        </div>
-                        <div class="col-xs-2"></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-9 col-sm-9">                              
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; 4Ps:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Non-4Ps:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; FBOs:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; PMC:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Usapan Serye:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Others:</p>
+                        <h5 style="color: red;"><b>TOTAL</b></h5>
                     </div>
-                    <div class="col-xs-12 margin-t15" style="padding: 0">
-                        <div class="col-xs-1 text-right" style="padding-left: 0; width: 1%">
-                            <span><i class="fa fa-bookmark-o"></i></span>
-                        </div>
-                        <div class="col-xs-9">
-                            <p class="small">September 3, 2019</p>
-                            <p class="small flex">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation
-                            </p>
-                        </div>
-                        <div class="col-xs-2"></div>
+                    <div class="col-xs-3 col-sm-3 text-right">
+                        <p>140,462</p>
+                        <p>60,817</p>
+                        <p>196</p>
+                        <p>45,671</p>
+                        <p>7,020</p>
+                        <p>16,366</p>
+                        <h5 style="color: red;"><b>555,460</b></h5>
                     </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <br>
+                        <h5>Intention to Use for Yr <?= $current_year; ?></h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-9 col-sm-9">
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Intention to Use FP Method</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Undecided</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Currently Pregnant</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; No Intention to Use FP Method</p>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 text-right">
+                        <p>27,427</p>
+                        <p>43,274</p>
+                        <p>6,607</p>
+                        <p>63,178</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <br>
                 </div>
             </div>
+            <div class="col-md-6 col-sm-12 col-xs-9"> 
+                <!-- <div class="row">
+                    <div class="col-xs-9 col-sm-9">
+                        <h5><b>Summary of FP Users</b></h5>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 text-right">
+                        
+                    </div>
+                </div> -->
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h5>Modern FP Users for Yr <?= $current_year; ?></h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-9 col-sm-9">
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Condom:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; IUD: </p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Pills:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Injectable:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Vasectomy:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Tubal Ligation:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Implant:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; CMM/Billings: </p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; BBT:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Sympto-Thermal:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; SDM:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; LAM:</p>
+                        <h5 style="color: red;"><b>TOTAL</b></h5>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 text-right">
+                        <p>10,681</p>
+                        <p>3,373</p>
+                        <p>6,415</p>
+                        <p>1,243</p>
+                        <p>1,106</p>
+                        <p>120,920</p>
+                        <p>10,681</p>
+                        <p>3,373</p>
+                        <p>6,415</p>
+                        <p>1,243</p>
+                        <p>1,106</p>
+                        <p>120,920</p>
+                        <h5 style="color: red;"><b>390,820</b></h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <br>
+                        <h5>Non-Modern FP Users for Yr <?= $current_year; ?></h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-9 col-sm-9">
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Withdrawal:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Rhythm: </p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Calendar:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Abstinence:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; Herbal:</p>
+                        <p><i class="fa fa-angle-double-right"></i> &nbsp; No Method:</p>
+                        <h5 style="color: red;"><b>TOTAL</b></h5>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 text-right">
+                        <p>10,681</p>
+                        <p>3,373</p>
+                        <p>6,415</p>
+                        <p>1,243</p>
+                        <p>1,106</p>
+                        <p>120,920</p>
+                        <h5 style="color: red;"><b>190,820</b></h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="row">
+            <form class="form-horizontal form-label-left">
+                <div class="form-group">
+                    <br>
+                    <label class="control-label col-md-9 col-sm-9 col-xs-12 text-right margin-t7">Choose Year</label>
+                    <div class="input-group col-md-3 col-sm-3 col-xs-12">
+                        <select class="form-control">
+                            <?php for ($i = $current_year; $i > 2011; $i--): ?>
+                                <option value="<?=$i?>"><?= $i?></option>
+                            <?php endfor?>
+                        </select>
+                        <span class="input-group-btn">
+                            <button type="button" class="btn btn-primary btn-hover">Go</button>
+                        </span>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="row">
+            <br>
+            <h4 class="text-center">PERCENTAGE OF COUPLES ENCODED FOR YEAR <?= $current_year; ?></h4>
+            <br><br>
+            <div id="graph"></div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="row">
+           <br>
+           <br>
+        </div>
+    </div> -->
+</div>
+<!-- <script>
+    loadJs(base_url + 'NewAssets/templateJs', function() {
+        loadJs(base_url + 'assets/js/graphJs/raphael-min.js', function() {
+            loadJs(base_url + 'assets/js/graphJs/morris.min.js', function() {
+                loadJs(base_url + 'assets/js/barGraph.js');
+            });
+        });
+    });  
+</script> -->
+
+
+
         </section>
 
         <footer>
