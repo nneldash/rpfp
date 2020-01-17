a4035f80831344edcbc6e9b6c05df2b1667cd4cf
welerz <reyes.rowel11@gmail.com>
2020-01-17 14:42:51 +0800

    prevent saving of service slip if no couple

--- application/controllers/Forms.php
+++ application/controllers/Forms.php
@@ -212,7 +212,7 @@ class Forms extends CI_Controller
         
         $slip = new ServiceSlipClass();
 
-        if (!$this->input->post('date_of_visit') || !$this->input->post('referral_facility') || !$this->input->post('health_service_provider')) {
+        if (!$this->input->post('date_of_visit') || !$this->input->post('referral_facility') || !$this->input->post('health_service_provider')|| $couple_id == 0) {
             return $slip;
         }
 
