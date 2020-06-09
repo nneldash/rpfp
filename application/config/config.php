73abf62a9f735ad0165913da7ec0184590c2d9d2
Ivan Alfonso <ppg_ivan@yahoo.com>
2020-06-09 22:46:41 +0800

    Change autoload, config, routes

--- application/config/config.php
+++ application/config/config.php
@@ -22,9 +22,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
-*/
-$config['base_url'] = 'http://[::1]/rpfp/';
+// */
+// $config['base_url'] = 'http://[::1]/rpfp/';
 
+$config['base_url'] = 'https://rpfp.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -35,7 +36,8 @@ $config['base_url'] = 'http://[::1]/rpfp/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+// $config['index_page'] = '';
+$config['index_page'] = 'views/index/landingPage.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -380,7 +382,8 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+// $config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
