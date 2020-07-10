893e76f904f4348530205c07b998df8053c188f3
ppg_ivan@yahoo.com <ppg_ivan@yahoo.com>
2020-07-10 14:08:11 +0800

    update config for server

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 // */
-$config['base_url'] = 'http://208.109.15.179/rpfp/';
+$config['base_url'] = 'http://[::1]/rpfp/';
+
+// $config['base_url'] = 'http://208.109.15.179/rpfp/';
 
 // $config['base_url'] = 'https://rpfp.herokuapp.com/';
 /*
@@ -528,7 +530,7 @@ $config['proxy_ips'] = '';
 
 $config[DB_DEFAULT] = array(
     'dsn'      => '',
-    'hostname' => 'localhost',
+    'hostname' => 'ip-208-109-15-179.ip.secureserver.net',
     USERNAME => '',
     USERPASSWORD => '',
     'database' => '',
